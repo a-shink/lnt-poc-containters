4f45d1a81f9447cca98c5ef50107418dc1676c45
Alexander Shinkarenko <ash@amvonet.com>
2019-08-07 17:39:20 +0700

    App: prepare files for API testing in Kube

--- app/prepare.sh
+++ app/prepare.sh
@@ -6,3 +6,11 @@ rm -rf htdocs/deploy/configs
 rm -rf htdocs/deploy/nginx-conf
 rm -rf htdocs/wp
 rm -f htdocs/.gitignore
+
+# Create tests/api.suite.yml by ENV vars for Kube
+cat htdocs/md/tests/api.suite.yml.template | sed \
+  -e 's/%DB_URL%/mysql:host=%DB_HOST%;dbname=%DB_NAME%/' >htdocs/md/tests/api.suite.yml
+
+# Point tests output to moodle data
+mv htdocs/md/codeception.yml htdocs/md/codeception.yml.orig
+cat htdocs/md/codeception.yml.orig | sed -e 's/output:.*$/output: \/vol\/mddata\/tests_output/' >htdocs/md/codeception.yml
